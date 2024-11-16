ad300315a572fc65b217fe3159b961698b4fafa8
Émilie Desmarais <emilied2002@gmail.com>
2024-11-16 15:45:49 -0500

    debut carrousel

---
+++ carrousel.php
@@ -0,0 +1,12 @@
+<?php
+/*
+plugin name: Carrousel
+description: Affiche un carrousel à partir d'une galerie d'image
+version: 1.0.0
+author: Eddy Martin
+author uri: https://referenced.ca
+*/
+
+function genere_carrousel(){
+    
+}
