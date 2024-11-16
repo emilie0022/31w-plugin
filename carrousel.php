226777f52e92bb5e360722a8ee5db525fabc0376
Émilie Desmarais <emilied2002@gmail.com>
2024-11-16 17:58:34 -0500

    modification des bouttons gauche et droite

--- carrousel.php
+++ carrousel.php
@@ -34,8 +34,11 @@ function genere_carrousel()
         '<button class="carrousel__bouton">Ouvrir</button>;
         <div class="carrousel">
             <button class="carrousel__X">X</button>
-            <button class="carrousel__gauche">gauche</button>
-            <button class="carrousel__droite">droite</button>
+            
+            <button class="carrousel__gauche"><svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#cceaff"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg></button>
+
+            <button class="carrousel__droite"><svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#cceaff"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></button>
+
         <figure class="carrousel__figure"></figure>
     </div>';
     return $chaine;
