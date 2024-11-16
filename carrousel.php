b460ad8cbba5254decce98388ab187cc9f0490d6
Émilie Desmarais <emilied2002@gmail.com>
2024-11-16 16:07:27 -0500

    creation du plugin

--- carrousel.php
+++ carrousel.php
@@ -7,6 +7,27 @@ author: Eddy Martin
 author uri: https://referenced.ca
 */
 
+function enfile_css_js()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "/style.css");
+    $version_js =  filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style(
+        "carrousel",
+        plugin_dir_url(__FILE__) . "/style.css",
+        array(),
+        $version_css
+    );
+    wp_enqueue_script(
+        "carrousel",
+        plugin_dir_url(__FILE__) . "js/carrousel.js",
+        array(),
+        $version_js,
+        true
+    );
+}
+add_action("wp_enqueue_scripts", "enfile_css_js");
+
 function genere_carrousel(){
-    
+
 }
